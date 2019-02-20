f796230f80790ff51205ed611880e27428c230e0
PioFederico <lovecchio.pio.federico@gmail.com>
2019-02-20 13:24:16 +0100

    Prima scrittura dell'esercizio

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <title>Hello</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <ul>
+            <?php
+            $A = ['mela','kiwi','pera','banana'];
+            for ($i=0,$i<count($A),$i++) {
+              print'<li>'.$A[$i].'</li>'
+            }
+            ?>
+            </ul>
+        </div>
+      </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.slim.min.js"> </script>
+    <script src="js/popper.min.js"> </script>
+    <script src="js/bootstrap.min.js"> </script>
+  </body>
+</html>
